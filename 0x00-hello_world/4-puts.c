4c1217b26770c52ffa8bd9cb8ab23bc622ffc400
azte-c <masidaniel02@gmail.com>
2023-06-23 11:01:17 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point 
- *
- * Return: Always 0 (Success)
- */
+*main- prints out the given statement 
+*Return: 0 (success)
+*/
+
 int main(void)
 {
-	puts("\"Programming is like buiding a multilingual puzzle");
-        return (0);
+puts("\"Programming is like buiding a multilingual puzzle");
+return (0);
 }
