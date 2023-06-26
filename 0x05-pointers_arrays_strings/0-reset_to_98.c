b0a5f05506913552912cce97e4671b0837aa2a7d
azte-c <masidaniel02@gmail.com>
2023-06-26 23:18:55 +0300

    function parameters

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * this function takes a pointer and changes the value of the number pointed to by the pointer
+ * Return: 0 success
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
