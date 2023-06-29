f28a5cd10f729bb17120cc53b0746518321a8ebd
azte-c <masidaniel02@gmail.com>
2023-06-30 01:25:59 +0300

    coppies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,7 @@ src_len++;
 for (index = 0; src[index] && index < n; index++)
 dest[index] = src[index];
 for (index = src_len; index < n; index++)
-dest[index] ='\0;;
+dest[index] ='\0';
 
 return (dest);
 }
