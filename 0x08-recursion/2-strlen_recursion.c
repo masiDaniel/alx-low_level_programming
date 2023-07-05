7fbd27976a5738bde5bdbb69cfabf84618348c10
azte-c <masidaniel02@gmail.com>
2023-07-05 12:34:20 +0300

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - checks the length of a string
+ * @s: the string to be checked
+ * Return: n the number of characters
+ */
+
+int _strlen_recursion(char *s)
+{
+int n = 0;
+if (*s)
+{
+n++;
+n += _strlen_recursion(s + 1);
+}
+return (n);
+}
