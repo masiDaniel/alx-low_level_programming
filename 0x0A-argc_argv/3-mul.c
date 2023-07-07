03b90142170fd7c920af3ac86425234f0e0c39b5
azte-c <masidaniel02@gmail.com>
2023-07-08 01:56:31 +0300

    multiplication and change

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,5 @@
-#include "main.h"
 #include <stdio.h>
-
-int _atoi(char *s);
-
-/**
- * main - multiplies two numbers
- * @argc: no of arguments
- * @argv: array of argumrnts
- * Return: 0 when not succesfull otherwise 1
- */
-
-int main (int argc, char *argv[])
-{
-int num1,num2,result;
-if (argc <= 2 || argc >= 4)
-printf("error\n");
-return (1);
-num1 = _atoi(argv[1]);
-num2 = _atoi(argv[2]);
-result = num1 * num2;
-
-printf("%d\n",result);
-return (0);
-}
-
+#include "main.h"
 
 /**
  * _atoi - converts a string to an integer
@@ -33,38 +9,65 @@ return (0);
  */
 int _atoi(char *s)
 {
-int i, d, n, len, f, digit ;
-i = 0;
-d = 0;
-len = 0;
-f = 0 ;
-n = 0;
-digit = 0;
-while (s[len] != '\0')
-len++;
+	int i, d, n, len, f, digit;
 
-while (i < len && f == 0)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
 {
-if (s[i] == '-')
-++d;
+	int result, num1, num2;
 
-if (s[i] >= '0' && s[i] <= '9')
-{
-digit = s[i] - '0';
-if (d % 2)
-digit = -digit;
-n = n * 10 + digit;
-f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
-break;
-f = 0;
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
-i++;
-}
-
-if (f == 0)
-return (0);
-
-return (n);
-}
-
