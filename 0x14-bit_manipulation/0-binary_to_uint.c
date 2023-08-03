75c92281abade88a0f745b669ef4cdf7d32a0ccd
azte-c <masidaniel02@gmail.com>
2023-08-04 02:13:45 +0300

    corecction on discription

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * binary_to_unit - convert binary no to unsigned int
+ * binary_to_uint - convert binary no to unsigned int
  * @b: string containing binary no
  * Return: unsigned int no
  */
 
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int no = 0;
 	int x = 0;
