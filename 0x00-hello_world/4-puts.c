a1a66e3b3ed60934cbb780654e2647f35a1236c5
masiDaniel <masidaniel02@gmail.com>
2024-03-11 17:01:10 +0300

    minor changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-*main- prints out the given statement 
-*Return: 0 (success)
-*/
-
+ *main - main entry point
+ *
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
-puts("\"Programming is like buiding a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 /**
- * main - Entry point 
+ *main -entry point
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	print("size of a char: %d byte(s)\n",sizeof(char));
-	print("size of an int: %d byte(s)\n",sizeof(int));
-	print("size of a long int: %d byte(s)\n",sizeof(long int));
-	print("size of a long long int: %d byte(s)\n",sizeof(long long int));
-	print("size of a float: %d byte(s)\n",sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
+}
