35402eb20a3ff92926d4617c0b98640df63ff2ed
masiDaniel <masidaniel02@gmail.com>
2024-04-19 00:43:38 +0300

    refreshing on the knowledge of recursion and styling the code effectively

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _print_rev_recursion - prints a string provided in reverse
+ * this is because the _putchar statts printing only after it has gotten to the end of line
  * @s: the string provided
  * Return: none
  */
